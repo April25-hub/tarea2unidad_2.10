2d3223046125b3fd6d1ed7f261e6a09fce85ab41
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-10-10 01:25:03 -0600

    Add files via upload

---
+++ main 10.py
@@ -0,0 +1,18 @@
+print(" ")
+print("Arzaba Diaz April 3W 1173")
+def es_vocal(caracter):
+    """
+    Esta funcion recibe un caracter y devuelve True si es una vocal, de lo contrario devuelve False.
+    
+    Args:
+    caracter (str): Un caracter a evaluar.
+    
+    Returns:
+    bool: True si el caracter es una vocal, False en caso contrario.
+    """
+    vocales = 'aeiouAEIOU'
+    return caracter in vocales
+
+
+
+  
